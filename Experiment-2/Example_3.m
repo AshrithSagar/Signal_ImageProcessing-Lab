81f21a39dd917b3c48c2b82aee534fb7c0358a5c
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-02-19 13:01:31 +0530

    E2_E3 1

---
+++ Experiment-2/Example_3.m
@@ -0,0 +1,37 @@
+%% Experiment - 2
+% Example - 3
+clc;
+clear;
+figure;
+
+%% With using inbuilt function
+n = -100 : 100;  % Discrete time index
+f_1 = 0.01;  % Normalised frequency 1
+f_2 = 0.02;  % Normalised frequency 2
+
+NUM = [2.2403, 2.4908, 2.2403];  % Numerator coefficients of T[z]
+DEN = [1, -0.4, 0.75];  % Denominator coefficients of T[z]
+
+x_1 = cos(2 * pi * f_1 * n);
+x_2 = cos(2 * pi * f_2 * n);
+
+c_1 = 2;
+c_2 = 3;
+
+x = c_1 * x_1 + c_2 * x_2;  % Linear combination of x_1 and x_2
+
+y_1 = filter(NUM, DEN, x_1);
+y_2 = filter(NUM, DEN, x_2);
+y_c = c_1 * y_1 + c_2 * y_2;  % Output of combined individual outputs
+y = filter(NUM, DEN, x);  % Output of combined inputs
+y_d = y - y_c;
+
+hold on;
+stem(n, y_c, 'filled')
+stem(n, y, 'filled')
+stem(n, y_d, 'filled')
+hold off;
+title('Linearity property of LSI systems')
+legend('y_c', 'y', 'y_d')
+xlabel('n')
+ylabel('Amplitude')
