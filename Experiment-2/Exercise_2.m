04e4b1b0324aec40a4572ed15b57185838a81cec
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-02-23 19:51:06 +0530

    E2_X2 1

---
+++ Experiment-2/Exercise_2.m
@@ -0,0 +1,18 @@
+%% Experiment - 2
+% Exercise - 2
+clc;
+clear;
+figure;
+
+n = -100 : 100;  % Discrete time index
+
+NUM = [2.2403, 2.4908, 2.2403];  % Numerator coefficients of T[z]
+DEN = [1, -0.4, 0.75];  % Denominator coefficients of T[z]
+
+h = impz(NUM, DEN);
+
+stem(n, h, 'filled')
+title('Impulse response of LSI system', 'Ashrith 200902016')
+legend('h')
+xlabel('n')
+ylabel('Amplitude')
