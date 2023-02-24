d8e1a1fcc6bda15670bdaf2fa0a398e6dac5b2f6
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-02-24 07:44:13 +0530

    E2_E8 2

--- Experiment-2/Example_8.m
+++ Experiment-2/Example_8.m
@@ -7,22 +7,27 @@ figure;
 %% M-point Moving Average Filter (Smoothing FIR Filter)
 n = -50 : 100;  % Discrete time index
 f_1 = 0.01;
-f_2 = 0.99;
+f_2 = 0.95;
 
 s_1 = cos(2 * pi * f_1 * n);  % Low frequency signal
 s_2 = cos(2 * pi * f_2 * n);  % High frequency signal
 x = s_1 + s_2;  % Input sequence
 
-M = 5;  % M-point Moving Average
-NUM = 1/M * ones(1, M);  % Numerator coefficients of T[z]
-DEN = [1];  % Denominator coefficients of T[z]
+Ms = [3, 5, 7];  % M-point Moving Average
+for M = Ms
+	NUM = 1/M * ones(1, M);  % Numerator coefficients of T[z]
+	DEN = [1];  % Denominator coefficients of T[z]
 
-h = impz(NUM, DEN);  % Impulse response
-y = conv(x, h);
+	h = impz(NUM, DEN);  % Impulse response
+	y_M = conv(x, h);
 
-hold on;
-stem(n, x, 'filled')
-stem(n, s_1, 'filled')
-stem(n, y_M, 'filled')
-legend('x', 's_1', 'y')
-hold off;
+	subplot(length(Ms), 1, find(M == Ms))
+	hold on;
+	stem(x, 'filled')
+	stem(s_1, 'filled')
+	stem(y_M, 'filled')
+	hold off;
+	legend('x', 's_1', 'y')
+	title(M + "-point Moving Average Filter (Smoothing FIR Filter)", 'Ashrith 200902016')
+	xlabel('n'), ylabel('Amplitude')
+end
