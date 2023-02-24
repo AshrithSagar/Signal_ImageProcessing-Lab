c21437151342c2e743f7dba66889d49f834905aa
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-02-24 22:08:31 +0530

    E4_X1 3

--- Experiment-4/Exercise_1.m
+++ Experiment-4/Exercise_1.m
@@ -7,11 +7,12 @@ figure;
 %% Low pass FIR filter design using the window method
 w_p = 0.375;  % Pass band frequency
 w_s = 0.5;  % Stop band frequency
+w_c = w_p + (w_s - w_p) / 2;  % Cut-off frequency
 
 k = 4;  % Hamming window
 N = ceil((2 * k * pi) / (w_s - w_p));  % Order of filter
 win = window(@hamming, N+1);  % Hamming window
-NUM = fir1(N, w_s, 'low', win);  % Numerator coefficients of T[z]
+NUM = fir1(N, w_c, 'low', win);  % Numerator coefficients of T[z]
 DEN = [1];  % Denominator coefficients of T[z]
 freqz(NUM, DEN)  % Frequency response
 
@@ -19,4 +20,4 @@ figure;
 H = freqz(NUM, DEN);  % Frequency response
 plot(abs(H))  % Magnitude spectrum
 title('Magnitude spectrum of Low pass FIR filter')
-xlabel('n'), ylabel('Amplitude')
+xlabel('frequency'), ylabel('Gain')
