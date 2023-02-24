34064f3c95a2c959ce284441694a8f9fceeee57e
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-02-24 07:52:26 +0530

    E2_X7 2

--- Experiment-2/Exercise_7.m
+++ Experiment-2/Exercise_7.m
@@ -6,9 +6,9 @@ figure;
 
 n = 0 : 10;  % Discrete time index
 x = [n == 0] + 3 * [n == 1] + 4 * [n == 3];  % Input signal
-h = (0.5 .^ n) .* (ge(n, 0) - ge(n, 5));
+h = (0.5 .^ n) .* (ge(n, 0) - ge(n, 5));  % Impulse response
 
-y = conv(x, h);
+y = conv(x, h);  % System output
 
 subplot(311)
 stem(x, 'filled')
