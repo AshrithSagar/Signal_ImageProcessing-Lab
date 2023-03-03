001c534170cf66b8e6c74b82a695bade35c2dc4f
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-03-03 15:29:28 +0530

    E4_X3 2

--- Experiment-4/Exercise_3.m
+++ Experiment-4/Exercise_3.m
@@ -2,7 +2,6 @@
 % Exercise - 3
 clc;
 clear;
-figure;
 
 %% Band pass FIR filter design using the window method
 Fs = 2000;  % Sampling frequency
@@ -11,7 +10,7 @@ w_ls = 200 / Fs;  % Lower stop band edge frequency
 w_up = 600 / Fs;  % Upper pass band edge frequency
 w_us = 700 / Fs;  % Upper stop band edge frequency
 w_lc = w_ls + (w_lp - w_ls) / 2;  % Lower cutoff frequency
-w_uc = w_up + (w_up - w_up) / 2;  % Upper cutoff frequency
+w_uc = w_up + (w_us - w_up) / 2;  % Upper cutoff frequency
 % Stop band attenuation = 50dB
 
 k = 4;  % Hamming window
@@ -19,10 +18,14 @@ N = ceil((2 * k * pi) / (w_us - w_up));  % Order of filter
 win = window(@hamming, N+1);  % Hamming window
 NUM = fir1(N, [w_lc, w_uc], 'bandpass', win);  % Numerator coefficients of T[z]
 DEN = [1];  % Denominator coefficients of T[z]
-freqz(NUM, DEN)  % Frequency response
+
+[H, W] = freqz(NUM, DEN);  % Frequency response
+
+figure; grid ON;
+plot(W / pi, 20 * log10(abs(H)))  % Magnitude spectrum
+title('Magnitude spectrum of Band pass FIR filter', "Ashrith 200902016");
+xlabel('Normalized Frequency  (\times\pi rad/sample)'), ylabel('Gain (dB)');
 
 figure;
-H = freqz(NUM, DEN);  % Frequency response
-plot(abs(H))  % Magnitude spectrum
-title('Magnitude spectrum of Band pass FIR filter')
-xlabel('frequency'), ylabel('Gain')
+zplane(NUM, DEN)
+title('Pole-Zero Plot', "Ashrith 200902016");
