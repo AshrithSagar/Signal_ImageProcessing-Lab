a742c73e3d2608c673a74271c7353f72ac1a5caf
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-03-03 15:23:53 +0530

    E4_X2 2

--- Experiment-4/Exercise_2.m
+++ Experiment-4/Exercise_2.m
@@ -2,7 +2,6 @@
 % Exercise - 2
 clc;
 clear;
-figure;
 
 %% High pass FIR filter design using the window method
 w_s = 0.2;  % Stop band frequency
@@ -14,10 +13,13 @@ N = ceil((2 * k * pi) / (w_p - w_s));  % Order of filter
 win = window(@hamming, N+1);  % Hamming window
 NUM = fir1(N, w_s, 'high', win);  % Numerator coefficients of T[z]
 DEN = [1];  % Denominator coefficients of T[z]
-freqz(NUM, DEN)  % Frequency response
+
+[H, W] = freqz(NUM, DEN);  % Frequency response
 
 figure;
-H = freqz(NUM, DEN);  % Frequency response
-plot(abs(H))  % Magnitude spectrum
-title('Magnitude spectrum of High pass FIR filter')
-xlabel('frequency'), ylabel('Gain')
+plot(W / pi, 20 * log10(abs(H)))  % Magnitude spectrum
+title('Magnitude spectrum of High pass FIR filter'); grid ON;
+xlabel('Normalized Frequency  (\times\pi rad/sample)'), ylabel('Gain (dB)')
+
+figure;
+zplane(NUM, DEN)
