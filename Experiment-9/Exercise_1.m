263e24184dc4fbe188f142288302f8c11cf400e8
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-07-23 12:06:08 +0530

    E9_X1 1

---
+++ Experiment-9/Exercise_1.m
@@ -0,0 +1,23 @@
+%% Experiment - 9
+% Exercise - 1
+clc;
+clear;
+
+%% Radon transform of a Modified Shepp-Logon Phantom
+img_phantom = phantom('Modified Shepp-Logan', 512);
+theta = 0 : 179;
+img_radon = radon(img_phantom, theta);
+% img_iradon = iradon(img_phantom, theta);
+
+%% Display image
+figure;
+imshow(img_phantom)
+title('Modified Shepp-Logon Phantom')
+
+figure;
+imshow(img_radon)
+title('Radon transform of a Modified Shepp-Logon Phantom')
+
+% figure;
+% imshow(img_iradon)
+% title('Reconstruction through Inverse Radon transform')
